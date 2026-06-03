cb2febde19795ac6d25cabc34b0c9a504126256f
AIML <aiemsaimlofficial@gmail.com>
2026-06-03 22:27:46 +0530

    Scaffold Vite React project for Vercel

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import SqlPlayground from './SqlPlayground'
+import './App.css'
+
+function App() {
+  return (
+    <div className="app-container">
+      <SqlPlayground />
+    </div>
+  )
+}
+
+export default App
